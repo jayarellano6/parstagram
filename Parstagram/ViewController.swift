d3881c9513d011c3ad5add9eace39d2c92449611
Jay Arellano <jaarellano@csumb.edu>
2019-02-25 14:11:31 -0800

    Initial Commit

---
+++ Parstagram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Parstagram
+//
+//  Created by Jay Arellano on 2/25/19.
+//  Copyright © 2019 Jay Arellano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
