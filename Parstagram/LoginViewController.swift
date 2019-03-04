e47e9a77bd59e0cf914b4afd7b54c0fbc4b6ab5a
Jay Arellano <jaarellano@csumb.edu>
2019-03-04 10:53:09 -0800

    done part

---
+++ Parstagram/LoginViewController.swift
@@ -0,0 +1,67 @@
+//
+//  LoginViewController.swift
+//  Parstagram
+//
+//  Created by Jay Arellano on 2/25/19.
+//  Copyright © 2019 Jay Arellano. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func onTap(_ sender: Any) {
+        print("tapping on main view")
+        view.endEditing(true);
+    }
+    
+    @IBAction func onSignIn(_ sender: Any) {
+        let username = usernameField.text!
+        let passwordF = passwordField.text!
+        
+        PFUser.logInWithUsername(inBackground: username, password: passwordF) {
+            (user, error)in
+            if user != nil {
+                // Do stuff after successful login.
+                self.performSegue(withIdentifier: "loginSegue", sender: nil)
+            } else {
+                // The login failed. Check error to see why.
+                print("Error: \(error?.localizedDescription)")
+            }
+        }
+    }
+    @IBAction func onSignUp(_ sender: Any) {
+        let user = PFUser()
+        user.username = usernameField.text
+        user.password = passwordField.text
+        
+        user.signUpInBackground { (success, error) in
+            if success{
+                self.performSegue(withIdentifier: "loginSegue", sender: nil)
+            }else {
+                print("error signing up: \(error?.localizedDescription)")
+            }
+        }
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
